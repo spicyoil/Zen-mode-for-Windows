4eed3a2eb8ac64fb1c4de868c3999a06dcaab8c2
spicyoil <zyj0818@gmail.com>
2019-12-07 19:42:29 +0900

    Update zenmode_pc.py

--- zenmode_pc.py
+++ zenmode_pc.py
@@ -1,17 +1,12 @@
 # make a  task in taskschd.msc  so every xx min the script will be run
-import ctypes 
-user32 = ctypes.windll.LoadLibrary('user32.dll')
 import time, datetime
-import win32api  
-WM_APPCOMMAND = 0x319
-APPCOMMAND_VOLUME_MAX = 0x0a
-APPCOMMAND_VOLUME_MIN = 0x09
+import subprocess
+
 
 
 today = datetime.datetime.fromtimestamp(time.time()) ## get time
 hour = int(today.strftime("%H")) ##get hour
-if (hour>=0) and (hour<=15):
-    #LOCK screen during night
-    user32.LockWorkStation()
-    #MUTE
-    win32api.SendMessage(-1, WM_APPCOMMAND, 0x30292, APPCOMMAND_VOLUME_MIN * 0x10000)
+if (hour>=0) and (hour<=19):
+    #Hibernate
+    subprocess.Popen("rundll32 powrprof.dll,SetSuspendState Hibernate")
+
